e4b1a234cc87033de8ee0b6d79630547bdfde431
Priscy Antonio Reales Mozo <realespriscy@hotmail.com>
2024-11-08 09:39:14 -0500

    🚀 Add microservices Catalog

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProdcut/CreateProductEndPoint.cs
@@ -0,0 +1,6 @@
+﻿namespace Catalog.API.Products.CreateProdcut
+{
+    public class CreateProductEndPoint
+    {
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProdcut/CreateProductHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Catalog.API.Products.CreateProdcut;
+
+public record CreateProductCommand(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price
+ );
+
+public record CreateProducResult(Guid Id);
+public class CreateProductHandler
+{
+}
