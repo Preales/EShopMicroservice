e1350332638ad5de1a690da79dcb922126d663a2
Priscy Antonio Reales Mozo <realespriscy@hotmail.com>
2024-11-11 10:14:16 -0500

    Add endPoint and logic in handler

--- src/Services/Catalog/Catalog.API/Products/CreateProdcut/CreateProductEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProdcut/CreateProductEndPoint.cs
@@ -1,6 +1,32 @@
-﻿namespace Catalog.API.Products.CreateProdcut
+﻿namespace Catalog.API.Products.CreateProdcut;
+
+public record CreateProductRequest(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price
+ );
+
+public record CreateProducResponse(Guid Id);
+
+public class CreateProductEndPoint : ICarterModule
 {
-    public class CreateProductEndPoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapPost("/products",
+            async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<CreateProducResponse>();
+
+                return Results.Created($"/products/{response.Id}", response);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProducResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithDescription("Create Product");
     }
 }
--- src/Services/Catalog/Catalog.API/Products/CreateProdcut/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProdcut/CreateProductHandler.cs
@@ -6,9 +6,25 @@ public record CreateProductCommand(
     string Description,
     string ImageFile,
     decimal Price
- );
+ ) : ICommand<CreateProducResult>;
 
 public record CreateProducResult(Guid Id);
-public class CreateProductHandler
+public class CreateProductHandler(IDocumentSession session)
+    : ICommandHandler<CreateProductCommand, CreateProducResult>
 {
+    public async Task<CreateProducResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+        return new CreateProducResult(product.Id);
+    }
 }
