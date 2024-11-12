0858211b8cf5d04b1b264b7a40adb7ba7fd07b93
Priscy Antonio Reales Mozo <realespriscy@hotmail.com>
2024-11-12 15:11:29 -0500

    ⚡Add Global Exception and Validator

--- src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -1,6 +1,10 @@
-﻿namespace Catalog.API.Exceptions;
+﻿using BuildingBlocks.Exceptions;
 
-public class ProductNotFoundException(Guid Id) : Exception
+namespace Catalog.API.Exceptions;
+
+public class ProductNotFoundException : NotFoundException
 {
-
+    public ProductNotFoundException(Guid Id) : base("Product", Id)
+    {
+    }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,20 +1,33 @@
+using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to container
 builder.Services.AddCarter();
+
+var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR(config =>
 {
-    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
+
+builder.Services.AddValidatorsFromAssembly(assembly);
+
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
 
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
 // configure the HTTP request pipeline
 app.MapCarter();
+
+app.UseExceptionHandler(options => { });
 
 app.Run();
