c21806dc58a76a66a6f5c4adc4506a42f6475f5a
Priscy Antonio Reales Mozo <realespriscy@hotmail.com>
2024-12-05 12:13:18 -0500

    ⚡Add abstract class like Aggregate - DomainEvent - Enitty

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Ordering.Domain.Abstractions;
+
+public interface IDomainEvent : INotification
+{
+    Guid EventId => Guid.NewGuid();
+    public DateTime OccurredOn => DateTime.UtcNow;
+    public DateTime OccurredOnLocalDate => DateTime.UtcNow.AddHours(-5);
+    public string EventType => GetType().AssemblyQualifiedName!;
+}
